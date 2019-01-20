59700f50a6b7d98226450b33e919c0637938c939
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-21 00:09:57 +0100

    Add a date suffix to the index

--- agent.sh
+++ agent.sh
@@ -3,11 +3,12 @@
 echo "Retrieving resources..."
 echo "Types: ${RESOURCE_TYPES}"
 
+SUFFIX=$(date +'%d-%m-%Y')
 kubectl get --all-namespaces --export --output=json "${RESOURCE_TYPES}" | jq -c '.items[]' | while IFS= read -r line; do
   echo "${line}" | curl \
     --insecure --basic --ssl-reqd \
     --request POST --header 'Content-Type: application/json' \
-    --data @- "${ELASTICSEARCH_INDEX_URL}/_doc" \
+    --data @- "${ELASTICSEARCH_INDEX_URL}-${SUFFIX}/_doc" \
       | jq '.'
   echo
 done
