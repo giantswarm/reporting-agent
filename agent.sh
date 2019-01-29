a642dec6d2129e2fb2cb2ec1825682e679856728
Fernando Ripoll <pipo02mix@gmail.com>
2019-01-29 14:42:36 +0100

    Add cluster metadata

--- agent.sh
+++ agent.sh
@@ -4,7 +4,10 @@ echo "Retrieving resources..."
 echo "Types: ${RESOURCE_TYPES}"
 
 SUFFIX=$(date +'%Y-%m-%d')
-kubectl get --all-namespaces --export --output=json "${RESOURCE_TYPES}" | jq -c '.items[]' | while IFS= read -r line; do
+kubectl get --all-namespaces --export --output=json "${RESOURCE_TYPES}" | \
+  jq -c '.items[]' | \
+  jq --arg cluster "${CLUSTER}" '.metadata.cluster = $cluster' | \
+  while IFS= read -r line; do
   echo "${line}" | curl \
     --insecure --basic --ssl-reqd \
     --request POST --header 'Content-Type: application/json' \
